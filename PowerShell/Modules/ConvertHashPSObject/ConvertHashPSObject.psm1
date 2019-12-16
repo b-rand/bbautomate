42a939d19b85d11c30361943f6be6929e04c047a
Brandon Bates <brandonbatesit@outlook.com>
2019-12-16 08:45:40 -0500

    Update ConvertHashPSObject.psm1

--- PowerShell/Modules/ConvertHashPSObject/ConvertHashPSObject.psm1
+++ PowerShell/Modules/ConvertHashPSObject/ConvertHashPSObject.psm1
@@ -26,15 +26,18 @@ function Convert-HashTablePSCustomObject {
     )
     #Determine if InputObject is of the correct type
     try {
+        #Determine if InputObject is of the correct type
         $type = $InputObject.GetType().Name
         if ($type -ne 'PSCustomObject' -and $type -ne 'Hashtable') {
             throw "Invalid Object Type"
         }
+        #Convert PSCustomObject to Hashtable
         if ($type -eq 'PSCustomObject') {
             $hashtable = @{ }
             $InputObject.psobject.properties | ForEach-Object { $hashtable[$_.Name] = $_.Value }
             return $hashtable
         }
+        #Convert Hashtable to PSCustomObject
         if ($type -eq 'Hashtable') {
             $psCustomObject = New-Object PSCustomObject
             foreach($key in $InputObject.keys){                
@@ -47,7 +50,7 @@ function Convert-HashTablePSCustomObject {
         if ($_.Exception.Message -eq "Invalid Object Type") {
             Write-Error $_.Exception.Message -Category InvalidType -CategoryReason "Object Must Be Of The Type Hashtable or Type PSCustomObject"            
         }
-        if ($_.Exception.Message -ne "Invalid Object Type"){
+        else ($_.Exception.Message -ne "Invalid Object Type"){
             Write-Error $_.Exception
         }
     }
